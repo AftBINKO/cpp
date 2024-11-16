0d80ca036a5038af2a7ff0acd206ba036ce53386
BINKO <chuyanov2006@gmail.com>
2024-11-16 13:14:56 +0300

    update

--- sequences/2/4.cpp
+++ sequences/2/4.cpp
@@ -1,30 +1,30 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main() {
-//    int n;
-//    double d, t;
-//    cin >> n;
-//    bool flag = true;
-//
-//    for (int i = 0; i < n; i++) {
-//        double a;
-//        cin >> a;
-//        if (i == 0) {
-//            t = a;
-//            continue;
-//        } else if (i == 1)
-//            d = a / t;
-//        else if ((a / t) != d)
-//            flag = false;
-//        t = a;
-//    }
-//
-//    if (flag)
-//        cout << "DA";
-//    else
-//        cout << "NET";
-//
-//    return 0;
-//}
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    double d, t;
+    cin >> n;
+    bool flag = true;
+
+    for (int i = 0; i < n; i++) {
+        double a;
+        cin >> a;
+        if (i == 0) {
+            t = a;
+            continue;
+        } else if (i == 1)
+            d = a / t;
+        else if ((a / t) != d)
+            flag = false;
+        t = a;
+    }
+
+    if (flag)
+        cout << "DA";
+    else
+        cout << "NET";
+
+    return 0;
+}
