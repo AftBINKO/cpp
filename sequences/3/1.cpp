185f15deb2839ec9fb8181389a4b9f3637e91343
BINKO <chuyanov2006@gmail.com>
2024-12-03 18:37:14 +0300

    3.3.1 completed

--- sequences/3/1.cpp
+++ sequences/3/1.cpp
@@ -4,26 +4,29 @@ using namespace std;
 
 int main() {
     int n, pre1, pre2, tmp, max_diff = 0, j;
+    bool flag;
     cin >> n;
 
     for (int i = 0; i < n; i++) {
         if (j == 0)
             cin >> pre1;
-        else if (j == 1)
+        else if (j == 1) {
             cin >> pre2;
-        else {
+            flag = pre2 <= pre1;
+        } else {
             cin >> tmp;
-            if (tmp > pre2)
+            if ((tmp > pre2 && !flag) || (tmp <= pre2 && flag))
                 pre2 = tmp;
             else {
-                pre1 = tmp;
-                j = 0;
+                pre1 = pre2;
+                pre2 = tmp;
+                flag = pre2 <= pre1;
+                j = 1;
             }
         }
 
-        if (j > 0 && (pre2 - pre1) > max_diff) max_diff = pre2 - pre1;
-
-        j++;
+        if (j > 0 && abs(pre2 - pre1) > max_diff) max_diff = abs(pre2 - pre1);
+        if (i == j || j != 1) j++;
     }
 
     cout << max_diff;
