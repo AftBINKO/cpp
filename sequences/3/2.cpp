39b240ebeb3d999ef938f0385f2beb04ab668472
BINKO <chuyanov2006@gmail.com>
2024-12-04 12:11:34 +0300

    2.3.2 completed

--- sequences/3/2.cpp
+++ sequences/3/2.cpp
@@ -3,24 +3,22 @@
 using namespace std;
 
 int main() {
-    int n, pre, a, count = 1, max_count = count;
+    int n, pre, a, count = 0, max_count = count;
     cin >> n;
 
     for (int i = 0; i < n; i++) {
-        if (i == 0)
+        if (i == 0) {
             cin >> pre;
-        else {
-            pre = a;
+            if (pre == 0) count++;
+        } else {
+            if (i > 1) pre = a;
             cin >> a;
 
-            if (pre == a)
-                count++;
-            else
-                count = 1;
-
-            if (count > max_count)
-                max_count = count;
+            if (pre == a && a == 0) count++;
+            else if (a == 0) count = 1;
+            else count = 0;
         }
+        if (count > max_count) max_count = count;
     }
 
     cout << max_count;
