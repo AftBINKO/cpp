7d27f1d555cf1388ece36dd36e52f29cd1be29ba
BINKO <chuyanov2006@gmail.com>
2024-12-09 23:28:46 +0300

    3.4.4 complete

---
+++ massives/4/4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, k, pre, t;
+    cin >> n >> k;
+    k %= n;
+
+    int *mas = new int[n];
+    for (int i = 0; i < n; i++)
+        cin >> mas[i];
+
+    if (k != 0 && n != 1) {
+        int *tmp = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (i - k < 0) tmp[n - abs(i - k)] = mas[i];
+            else tmp[i - k] = mas[i];
+        }
+
+        for (int i = 0; i < n; i++)
+            mas[i] = tmp[i];
+
+        delete[] tmp;
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << mas[i] << " ";
+
+    delete[] mas;
+
+    return 0;
+}
