2d1280c0bf3db7f1b24c673dd9e6102871efa96c
BINKO <chuyanov2006@gmail.com>
2024-12-10 18:25:48 +0300

    2.3.2 update

--- sequences/3/2.cpp
+++ sequences/3/2.cpp
@@ -3,21 +3,14 @@
 using namespace std;
 
 int main() {
-    int n, pre, a, count = 0, max_count = count;
+    int n, a, count = 0, max_count = count;
     cin >> n;
 
     for (int i = 0; i < n; i++) {
-        if (i == 0) {
-            cin >> pre;
-            if (pre == 0) count++;
-        } else {
-            if (i > 1) pre = a;
-            cin >> a;
+        cin >> a;
+        if (a == 0) count++;
+        else count = 0;
 
-            if (pre == a && a == 0) count++;
-            else if (a == 0) count = 1;
-            else count = 0;
-        }
         if (count > max_count) max_count = count;
     }
 
