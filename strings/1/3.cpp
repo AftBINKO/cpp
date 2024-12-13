cc3132bb5eb1e9523d028d74589301e216db6112
BINKO <chuyanov2006@gmail.com>
2024-12-14 01:17:11 +0300

    4.1 complete

--- strings/1/3.cpp
+++ strings/1/3.cpp
@@ -1,24 +1,24 @@
-//#include <iostream>
-//#include <cstring>
-//
-//using namespace std;
-//
-//int main() {
-//    const int MAX_LEN = 100, D = 256;
-//    int *mas = new int[D];
-//    for (int i = 0; i < D; i++) mas[i] = 0;
-//
-//    char str[MAX_LEN];
-//    cin.getline(str, MAX_LEN);
-//    auto l = (unsigned int) strlen(str);
-//
-//    for (unsigned int i = 0; i < l; i++)
-//        mas[(unsigned int) ((unsigned char) str[i])]++;
-//
-//    for (unsigned int i = 0; i < D; i++) {
-//        if (mas[i]) cout << (unsigned char) mas[i] << ' ' << mas[i] << endl;
-//    }
-//
-//    delete[] mas;
-//    return 0;
-//}
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    const int MAX_LEN = 100, D = 256;
+    int *mas = new int[D];
+    for (int i = 0; i < D; i++) mas[i] = 0;
+
+    char str[MAX_LEN];
+    cin.getline(str, MAX_LEN);
+    auto l = (unsigned int) strlen(str);
+
+    for (unsigned int i = 0; i < l; i++)
+        mas[(unsigned int) ((unsigned char) str[i])]++;
+
+    for (unsigned int i = 0; i < D; i++) {
+        if (mas[i]) cout << (unsigned char) mas[i] << ' ' << mas[i] << endl;
+    }
+
+    delete[] mas;
+    return 0;
+}
--- strings/1/4.cpp
+++ strings/1/4.cpp
@@ -1,23 +1,23 @@
-//#include <iostream>
-//#include <cstring>
-//
-//using namespace std;
-//
-//int main() {
-//    const int N = 100;
-//    char str[N];
-//    int cnt = 0;
-//
-//    cin.getline(str, N);
-//    auto l = (unsigned int) strlen(str);
-//
-//    for (int i = 0; i < l; i++) {
-//        if (str[i] == '(') cnt++;
-//        else if (str[i] == ')') cnt--;
-//    }
-//
-//    if (cnt == 0) cout << "OK" << endl;
-//    else cout << "NO" << endl;
-//
-//    return 0;
-//}
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    const int N = 100;
+    char str[N];
+    int cnt = 0;
+
+    cin.getline(str, N);
+    auto l = (unsigned int) strlen(str);
+
+    for (int i = 0; i < l; i++) {
+        if (str[i] == '(') cnt++;
+        else if (str[i] == ')') cnt--;
+    }
+
+    if (cnt == 0) cout << "OK" << endl;
+    else cout << "NO" << endl;
+
+    return 0;
+}
