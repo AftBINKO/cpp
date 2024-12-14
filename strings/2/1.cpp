abd29a924fbc87ba5f9c5d50a63091b572bd9fac
BINKO <chuyanov2006@gmail.com>
2024-12-14 13:40:37 +0300

    4.2 update

--- strings/2/1.cpp
+++ strings/2/1.cpp
@@ -1,18 +1,18 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main() {
-//    const int MAX_LEN = 100;
-//
-//    char str[MAX_LEN];
-//    cin.getline(str, MAX_LEN);
-//
-//    for (int i = 0; str[i] != '\0'; i++) {
-//        if (str[i] == ' ' && str[i + 1] != '\0' && str[i + 1] != ' ')
-//            cout << endl;
-//        else cout << str[i];
-//    }
-//
-//    return 0;
-//}
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int MAX_LEN = 100;
+
+    char str[MAX_LEN];
+    cin.getline(str, MAX_LEN);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ' && str[i + 1] != '\0' && str[i + 1] != ' ')
+            cout << endl;
+        else cout << str[i];
+    }
+
+    return 0;
+}
--- strings/2/4.cpp
+++ strings/2/4.cpp
@@ -1,37 +1,37 @@
-//#include <iostream>
-//#include <cstring>
-//
-//using namespace std;
-//
-//int main() {
-//    const int N = 100;
-//    char str[N];
-//
-//    cin.getline(str, N);
-//    auto l = (unsigned int) strlen(str);
-//
-//    int n, count = 0, k = 0, d, uk1, uk2;
-//    cin >> n;
-//
-//    while (k < l && count < n) {
-//        for (int i = k; !(str[i] == ' ' || str[i] == '\0') && count < n; i++)
-//            if (str[i + 1] == ' ' || str[i + 1] == '\0') {
-//                count++;
-//                if (count == n) {
-//                    uk1 = k;
-//                    uk2 = i;
-//                }
-//                k = i + 2;
-//            }
-//    }
-//
-//    for (int i = uk1; str[0] != '\0' && i <= uk2 + 1; i++) {
-//        for (int j = uk1; str[0] != '\0' && j < l - 1; j++)
-//            str[j] = str[j + 1];
-//        str[--l] = '\0';
-//    }
-//
-//    cout << str << endl;
-//
-//    return 0;
-//}
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    const int N = 100;
+    char str[N];
+
+    cin.getline(str, N);
+    auto l = (unsigned int) strlen(str);
+
+    int n, count = 0, k = 0, d, uk1, uk2;
+    cin >> n;
+
+    while (k < l && count < n) {
+        for (int i = k; !(str[i] == ' ' || str[i] == '\0') && count < n; i++)
+            if (str[i + 1] == ' ' || str[i + 1] == '\0') {
+                count++;
+                if (count == n) {
+                    uk1 = k;
+                    uk2 = i;
+                }
+                k = i + 2;
+            }
+    }
+
+    for (int i = uk1; str[0] != '\0' && i <= uk2 + 1; i++) {
+        for (int j = uk1; str[0] != '\0' && j < l - 1; j++)
+            str[j] = str[j + 1];
+        str[--l] = '\0';
+    }
+
+    cout << str << endl;
+
+    return 0;
+}
