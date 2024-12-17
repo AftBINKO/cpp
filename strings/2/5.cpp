d5a94b9ec7574a33e16c32e6eba5e0e8fbad5448
BINKO <chuyanov2006@gmail.com>
2024-12-18 00:24:45 +0300

    4.2.5 fixed

--- strings/2/5.cpp
+++ strings/2/5.cpp
@@ -16,6 +16,43 @@ int main() {
     cin.getline(word, N);
     auto l2 = (unsigned int) strlen(word);
 
+    for (int i = (int) l1 - 1; i >= 0 && str[i] == ' '; i--) str[i] = '\0';
+    for (int i = 0; str[i] != '\0'; i++)  // было немного лень избавляться от дублирования кода, но главное работает)
+        if (str[i] == ' ') {
+            if (i == 0) {
+                while (str[0] == ' ') {
+                    for (int j = 0; j < l1 - 1; j++)
+                        str[j] = str[j + 1];
+                    str[--l1] = '\0';
+                }
+            } else
+                while (str[i + 1] == ' ') {
+                    for (int j = i + 1; j < l1 - 1; j++)
+                        str[j] = str[j + 1];
+                    str[--l1] = '\0';
+                }
+        }
+
+    for (int i = (int) l2 - 1; i >= 0 && word[i] == ' '; i--) word[i] = '\0';
+    for (int i = 0; word[i] != '\0'; i++)
+        if (word[i] == ' ') {
+            if (i == 0) {
+                while (word[0] == ' ') {
+                    for (int j = 0; j < l2 - 1; j++)
+                        word[j] = word[j + 1];
+                    word[--l2] = '\0';
+                }
+            } else
+                while (word[i + 1] == ' ') {
+                    for (int j = i + 1; j < l2 - 1; j++)
+                        word[j] = word[j + 1];
+                    word[--l2] = '\0';
+                }
+        }
+
+    l1 = (unsigned int) strlen(str);
+    l2 = (unsigned int) strlen(word);
+
     while (k < l1) {
         flag = true;
         for (int i = k; flag && i < k + l2; i++) {
